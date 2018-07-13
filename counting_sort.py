47fd388a9a0faf9d7ab914fc6a0642f85c123dae
caoxiang104 <caoxiang112@gmail.com>
2018-07-13 21:58:40 +0800

    counting sort

---
+++ counting_sort.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+def counting_sort(a, key=lambda x: x):
+    b, c = [], defaultdict(list)
+    for x in a:
+        c[key(x)].append(x)
+    for k in range(min(c), max(c) + 1):
+        b.extend(c[k])
+    return b
+
+
+def main():
+    seq = [1, 5, 3, 4, 5, 1000, 2]
+    seq = counting_sort(seq)
+    print("".join(str(seq)))
+
+
+if __name__ == '__main__':
+    main()

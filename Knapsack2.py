53e812b66bd26a6731ac91ca23d17403dd85f372
caoxiang104 <caoxiang112@gmail.com>
2018-07-25 13:49:59 +0800

    完全背包问题

--- Knapsack2.py
+++ Knapsack2.py
@@ -1,3 +1,9 @@
+# -*- coding:utf-8 -*-
+"""完全背包表示每个物品可以取无限次，只要加起来总容量不超过V就可以。
+同样可以用f[i][j]表示前i间物品恰放入一个容器为j的背包可以获得的最大价值。则其状态转移方程为：
+f[i][j] = max{f[i-1][j-k*weight[i]]+k*value[i]} ,其中(0<=k<=j/weight[i]) """
+
+
 def knapsack(w, v, N, c):
     for j in range(1, c + 1):
         for i in range(1, len(w)):
